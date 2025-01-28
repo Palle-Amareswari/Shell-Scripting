20fa296da4bef6e559da63d952220f6325ab7ed2
Palle-Amareswari <92499127+Palle-Amareswari@users.noreply.github.com>
2025-01-28 23:38:11 +0530

    exercise-6.sh

---
+++ exercise-6.sh
@@ -0,0 +1,35 @@
+Exercise-6
+
+#Write a shell script that prompts the user for a name of a file or directory and reports if it is a
+#regular file, a directory, or other type of file.
+#Also perform an ls command against the file or directory with the long listing option.
+
+
+
+
+#!/bin/bash
+
+read -p "Enter the name of a file or directory: " path
+
+if [ -e "$path" ]; then
+    if [ -f "$path" ]; then
+        echo "$path is a regular file."
+    elif [ -d "$path" ]; then
+        echo "$path is a directory."
+    elif [ -b "$path" ]; then
+        echo "$path is a blocked file."
+    elif [ -c "$path" ]; then
+        echo "$path is a character file."
+    else
+        echo "$path is of another type."
+    fi
+
+    ls -l "$path"
+else
+    echo "$path does not exist."
+fi
+
+
+To run this file:
+# > chmod +x exercise_6.sh
+# > sh exercise_6.sh
