bd2f9bc2a662a46107b974ae356b28cf47e32033
Palle-Amareswari <92499127+Palle-Amareswari@users.noreply.github.com>
2025-01-28 23:06:17 +0530

    exercise_4.sh

---
+++ exercise_4.sh
@@ -0,0 +1,54 @@
+Exercise - 4 :
+-------------
+Write a shell script to check to see if the file "/etc/shadow" exists. If it does exist, display
+"Shadow passwords are enabled." Next, check to see if you can write to the file. If you can,
+display "You have permissions to edit /etc/shadow." If you cannot, display "You do NOT have
+permissions to edit /etc/shadow."
+
+
+#! /bin/bash
+
+path="/etc/shadow"
+
+if [ -e "$path" ]; then
+      echo "Shadow passwords are enabled."
+
+      if [ -w "$path" ]; then
+            echo "You have permissions to edit /etc/shadow."
+
+      else
+             echo "You do NOT have permissions to edit /etc/shadow."
+
+      fi
+else
+      echo "The file /etc/shadow doesn't exists"
+
+#fi 
+
+               or
+
+
+#! /bin/bash
+
+path="/etc/shadow1"
+
+if [ -e "$path" ]; then
+       echo "Shadow passwords are enabled."
+
+      if [ -w "$path" ]; then
+             echo "You have permissions to edit /etc/shadow."
+
+      else
+             echo "You do NOT have permissions to edit /etc/shadow."
+
+      fi
+else
+        echo "The file /etc/shadow doesn't exists"
+
+fi
+
+
+# To run this file:
+
+# > chmod +x exercise-4.sk
+# > sh exercise-4.sh
