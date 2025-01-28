d96c6076eaf79589113fcf95e9662af4a77bf186
Palle-Amareswari <92499127+Palle-Amareswari@users.noreply.github.com>
2025-01-28 21:47:32 +0530

    exercise-2.sh

---
+++ exercise-2.sh
@@ -0,0 +1,15 @@
+Exercise - 2
+Modify the shell script from exercise 1 to include a variable. The variable will hold the contents
+of the message "Shell Scripting is Fun!".
+
+Sol:
+
+#! /bin/bash
+x="Shell Scripting is Fun!"
+echo $x
+
+
+#To run this file :
+
+# > chmod +x exercise-2.sh
+# > sh exercise-2.sh
