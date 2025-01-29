1f5cdf744bfcc01d64eb0f779bf696bd9c1b5cf3
Palle-Amareswari <92499127+Palle-Amareswari@users.noreply.github.com>
2025-01-29 10:18:56 +0530

    exercise-6.sh

--- exercise-6.sh
+++ exercise-6.sh
@@ -1,11 +1,13 @@
 Exercise-6
 
-#Write a shell script that prompts the user for a name of a file or directory and reports if it is a
-#regular file, a directory, or other type of file.
-#Also perform an ls command against the file or directory with the long listing option.
+Write a shell script that prompts the user for a name of a file or directory and reports if it is a
+regular file, a directory, or other type of file.
+Also perform an ls command against the file or directory with the long listing option.
 
 
 
+
+Sol:
 
 #!/bin/bash
 
